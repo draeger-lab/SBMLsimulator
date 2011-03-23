f5e09a861b66299de0ae75a521b99913c9db766a
Roland Keller <rolandkeller5@googlemail.com>
2011-03-23 09:22:30 +0000

    new distance functions added

---
+++ src/org/sbml/simulator/math/Euclidean.java
@@ -0,0 +1,66 @@
+/*
+ * $Id:  Euclidean.java 09:56:46 keller$
+ * $URL: Euclidean.java $
+ * ---------------------------------------------------------------------
+ * This file is part of SBMLsimulator, a Java-based simulator for models
+ * of biochemical processes encoded in the modeling language SBML.
+ *
+ * Copyright (C) 2007-2011 by the University of Tuebingen, Germany.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+
+package org.sbml.simulator.math;
+
+/**
+ * @author Roland Keller
+ * @version $Rev$
+ * @since
+ */
+public class Euclidean extends NMetric {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2520265250898674233L;
+	
+	/**
+	 * 
+	 */
+	public Euclidean() {
+		super(2d);
+	}
+	
+	/**
+	 * Set the current root to be used in the distance function to the specified
+	 * value.
+	 * 
+	 * @param root
+	 */
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sbml.squeezer.math.Distance#setRoot(double)
+	 */
+	@Override
+	public void setRoot(double root) {
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sbml.squeezer.math.Distance#getName()
+	 */
+	@Override
+	public String getName() {
+		return "Euclidean";
+	}
+
+
+}
---
+++ src/org/sbml/simulator/math/RelativeManhattan.java
@@ -0,0 +1,47 @@
+/*
+ * $Id:  RelativeManhattan.java 10:15:03 keller$
+ * $URL: RelativeManhattan.java $
+ * ---------------------------------------------------------------------
+ * This file is part of SBMLsimulator, a Java-based simulator for models
+ * of biochemical processes encoded in the modeling language SBML.
+ *
+ * Copyright (C) 2007-2011 by the University of Tuebingen, Germany.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+
+package org.sbml.simulator.math;
+
+/**
+ * @author Roland Keller
+ * @version $Rev$
+ * @since
+ */
+public class RelativeManhattan extends RelativeNMetric {
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5446188147827736620L;
+
+	public RelativeManhattan() {
+		super();
+		metric=new Manhattan();
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.sbml.squeezer.math.Distance#setRoot(double)
+	 */
+	@Override
+	public void setRoot(double root) {
+	}
+}
