c322ade61072b18b6b2385d1a5fcf7863c6ed524
Roland Keller <roland.keller@gmx.de>
2011-07-27 12:05:42 +0000

    Model merging class added

--- src/org/sbml/simulator/util/AnnotationUtils.java
+++ src/org/sbml/simulator/util/AnnotationUtils.java
@@ -18,7 +18,6 @@
 package org.sbml.simulator.util;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
