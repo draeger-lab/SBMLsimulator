ece87fa647502c8d6ca2fe94a6bb9795a6c9a9c1
Robin Tim Fähnrich <robin-tim.faehnrich@student.uni-tuebingen.de>
2012-11-13 12:49:48 +0000

    Interface TargetFunction added to have the possibility implement classes like FluxMinimization. Class FluxMinimization added to optimize the flux minimization problem.

---
+++ src/org/sbml/simulator/fba/dynamic/FluxMinimization.java
@@ -0,0 +1,51 @@
+/*
+ * $Id:  FluxMinimization.java 11:34:24 Robin$
+ * $URL$
+ * ---------------------------------------------------------------------
+ * This file is part of SBMLsimulator, a Java-based simulator for models
+ * of biochemical processes encoded in the modeling language SBML.
+ *
+ * Copyright (C) 2007-2011 by the University of Tuebingen, Germany.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+package org.sbml.simulator.fba.dynamic;
+
+/**
+ * @author Robin F&auml;hnrich
+ * @version $Rev$
+ * @since 1.0
+ */
+public class FluxMinimization implements TargetFunction {
+	
+	/**
+	 * @return a computed flux vector optimized by CPLEX
+	 */
+	public double[] getOptimizedFluxVector() {
+		// TODO implement method
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.sbml.simulator.fba.dynamic.TargetFunction#isMinProblem()
+	 */
+	@Override
+	public boolean isMinProblem() {
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.sbml.simulator.fba.dynamic.TargetFunction#isMaxProblem()
+	 */
+	@Override
+	public boolean isMaxProblem() {
+		return false;
+	}
+	
+}
