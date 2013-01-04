3812d8d290bf7ab4f0d378955d36433d3ab4a387
Andreas Dräger <andreas.draeger@uni-tuebingen.de>
2013-01-04 11:46:00 +0000

    Updated license headers to 2013. Happy new year!

--- SED-ML-support/src/org/simulator/sedml/SimulatorTest.java
+++ SED-ML-support/src/org/simulator/sedml/SimulatorTest.java
@@ -1,14 +1,29 @@
-package org.simulator.sedml;
-
 /*
- * $Id: SimulatorTest.java 15:40:12 draeger$ $URL:
+ * $Id$
+ * $URL$
+ * ---------------------------------------------------------------------
+ * This file is part of SBMLsimulator, a Java-based simulator for models
+ * of biochemical processes encoded in the modeling language SBML.
+ *
+ * Copyright (C) 2007-2013 by the University of Tuebingen, Germany.
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation. A copy of the license
+ * agreement is provided in the file named "LICENSE.txt" included with
+ * this software distribution and also available online as
+ * <http://www.gnu.org/licenses/lgpl-3.0-standalone.html>.
+ * ---------------------------------------------------------------------
+ */
+package org.simulator.sedml;
+/*
  * https://sbml-simulator.svn.sourceforge
  * .net/svnroot/sbml-simulator/trunk/test/org/simulator/SimulatorTest.java $
  * --------------------------------------------------------------------- This
  * file is part of SBMLsimulator, a Java-based simulator for models of
  * biochemical processes encoded in the modeling language SBML.
  * 
- * Copyright (C) 2007-2012 by the University of Tuebingen, Germany.
+ * Copyright (C) 2007-2013 by the University of Tuebingen, Germany.
  * 
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
@@ -19,7 +34,6 @@ package org.simulator.sedml;
  */
 
 import java.awt.Dimension;
-import java.io.File;
 import java.io.IOException;
 
 import javax.swing.JOptionPane;
@@ -44,7 +58,7 @@ import uk.ac.ed.csbe.sbsivisual.sbsiDataFormat.SBSIDataBuilder;
  * A simple program that performs a simulation of a model.
  * 
  * @author Andreas Dr&auml;ger
- * @version $Rev: 72 $
+ * @version $Rev$
  * @since 0.9
  */
 public class SimulatorTest {
