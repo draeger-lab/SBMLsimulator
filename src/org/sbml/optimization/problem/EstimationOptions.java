453a5ac9b424f84066a4d827e5f3aac0683f7dfc
Andreas Dräger <draeger@informatik.uni-tuebingen.de>
2013-01-04 12:01:58 +0000

    Removed unnecessary package.html files and updated license headers to 2013. Happy new year!

--- src/org/sbml/optimization/problem/EstimationOptions.java
+++ src/org/sbml/optimization/problem/EstimationOptions.java
@@ -5,7 +5,7 @@
  * This file is part of Simulation Core Library, a Java-based library for
  * of biochemical processes encoded in the modeling language SBML.
  *
- * Copyright (C) 2007-2012 by the University of Tuebingen, Germany.
+ * Copyright (C) 2007-2013 by the University of Tuebingen, Germany.
  *
  * This library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
